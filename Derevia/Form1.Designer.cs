53f0520881d208ba1c7899e649c015c97f17ac14
Psareva Alina <abashacme@mail.ru>
2020-05-24 21:33:35 +0300

    Новый дизайн новая жинь

--- Derevia/Form1.Designer.cs
+++ Derevia/Form1.Designer.cs
@@ -76,9 +76,9 @@
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(25, 58);
+            this.groupBox1.Location = new System.Drawing.Point(375, 47);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(113, 170);
+            this.groupBox1.Size = new System.Drawing.Size(113, 165);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Выберите дерево";
@@ -87,7 +87,7 @@
             // 
             this.radioButton4.AutoSize = true;
             this.radioButton4.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.radioButton4.Location = new System.Drawing.Point(19, 146);
+            this.radioButton4.Location = new System.Drawing.Point(19, 134);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(46, 17);
             this.radioButton4.TabIndex = 3;
@@ -98,7 +98,7 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.radioButton3.Location = new System.Drawing.Point(19, 111);
+            this.radioButton3.Location = new System.Drawing.Point(19, 99);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(47, 17);
             this.radioButton3.TabIndex = 2;
@@ -109,7 +109,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.radioButton2.Location = new System.Drawing.Point(19, 78);
+            this.radioButton2.Location = new System.Drawing.Point(19, 66);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(46, 17);
             this.radioButton2.TabIndex = 1;
@@ -121,7 +121,7 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.BackgroundImage = global::Derevia.Properties.Resources.bbb;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(19, 45);
+            this.radioButton1.Location = new System.Drawing.Point(19, 33);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(68, 17);
             this.radioButton1.TabIndex = 0;
@@ -131,9 +131,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(188, 113);
+            this.richTextBox1.Location = new System.Drawing.Point(28, 68);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(309, 30);
+            this.richTextBox1.Size = new System.Drawing.Size(328, 30);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
@@ -148,7 +148,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(510, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -337,7 +337,7 @@
             // button1
             // 
             this.button1.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.button1.Location = new System.Drawing.Point(174, 250);
+            this.button1.Location = new System.Drawing.Point(28, 220);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 58);
             this.button1.TabIndex = 4;
@@ -348,7 +348,7 @@
             // button2
             // 
             this.button2.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.button2.Location = new System.Drawing.Point(338, 250);
+            this.button2.Location = new System.Drawing.Point(197, 220);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 58);
             this.button2.TabIndex = 5;
@@ -359,7 +359,7 @@
             // button3
             // 
             this.button3.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.button3.Location = new System.Drawing.Point(507, 251);
+            this.button3.Location = new System.Drawing.Point(27, 136);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(144, 57);
             this.button3.TabIndex = 6;
@@ -369,7 +369,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(533, 113);
+            this.textBox1.Location = new System.Drawing.Point(216, 164);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 8;
@@ -378,9 +378,9 @@
             // button4
             // 
             this.button4.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.button4.Location = new System.Drawing.Point(42, 252);
+            this.button4.Location = new System.Drawing.Point(375, 238);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(86, 54);
+            this.button4.Size = new System.Drawing.Size(113, 40);
             this.button4.TabIndex = 9;
             this.button4.Text = "Перезапуск программы";
             this.button4.UseVisualStyleBackColor = true;
@@ -391,7 +391,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.LightCyan;
             this.label1.Image = global::Derevia.Properties.Resources.bbb;
-            this.label1.Location = new System.Drawing.Point(185, 81);
+            this.label1.Location = new System.Drawing.Point(25, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 10;
@@ -402,7 +402,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Image = global::Derevia.Properties.Resources.bbb;
-            this.label2.Location = new System.Drawing.Point(539, 81);
+            this.label2.Location = new System.Drawing.Point(213, 144);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 11;
@@ -414,7 +414,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Derevia.Properties.Resources.bbb;
-            this.ClientSize = new System.Drawing.Size(683, 326);
+            this.ClientSize = new System.Drawing.Size(510, 299);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
